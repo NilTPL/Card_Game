bd117a42f3eb57fb82c794235ab16f09f1b7cdc5
NilTPL <76227208+NilTPL@users.noreply.github.com>
2024-11-13 06:56:29 +0100

    redoall

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 ﻿#include "stdafx.h"
-#include "Game.h"
 #include <iostream>
 
 
@@ -10,19 +9,48 @@ int main()
 	//Init srand
 	std::srand(time(nullptr));
 
-	//Init game
-	Game game;
+	//init rest
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Blackjack in C++", sf::Style::Titlebar | sf::Style::Close, settings);
+
+	sf::CircleShape shape(140.f, 8);
+	shape.setFillColor(sf::Color(0, 60, 70));
+	shape.setOrigin(sf::Vector2f(sf::Vector2i(shape.getLocalBounds().getSize() / 2.f + shape.getLocalBounds().getPosition())));
+	shape.setPosition(sf::Vector2f(sf::Vector2i(window.getView().getSize() / 2.f)));
+	
+	
+	sf::Font consolasFont;
+	consolasFont.loadFromFile("fonts/Consolas.ttf");
+	
+	sf::Text startText("words on a screen", consolasFont, 32);
+	startText.setOrigin(sf::Vector2f(sf::Vector2i(startText.getLocalBounds().getSize() / 2.f + startText.getLocalBounds().getPosition())));
+	startText.setPosition(sf::Vector2f(sf::Vector2i(window.getView().getSize() / 2.f)));
 
 
-	//Game loop
-	while (game.running()) // do these \/ each frame
+	//main loop
+
+	while (window.isOpen())
 	{
-		//Update
-		game.update();
-		
-		//Render
-		game.render();
-	};
+		//UPDATE
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Keyboard::Escape)
+				window.close();
+		}
+
+
+		//DRAW
+		window.clear(sf::Color(20, 30, 50));
+		window.draw(shape);
+		window.draw(startText);
+		window.display();
+
+	}
+
 
 	//End of application
 	return 0;
