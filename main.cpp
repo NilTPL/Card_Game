3034c41f8e04e65a7d3be1ee2050123513442677
NilT_PL <wojciech.tomaszewski07@gmail.com>
2024-11-14 00:13:35 +0100

    mouse click detection

--- main.cpp
+++ main.cpp
@@ -13,11 +13,6 @@ int main()
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Blackjack in C++", sf::Style::Titlebar | sf::Style::Close, settings);
-
-	sf::CircleShape shape(140.f, 8);
-	shape.setFillColor(sf::Color(0, 60, 70));
-	shape.setOrigin(sf::Vector2f(sf::Vector2i(shape.getLocalBounds().getSize() / 2.f + shape.getLocalBounds().getPosition())));
-	shape.setPosition(sf::Vector2f(sf::Vector2i(window.getView().getSize() / 2.f)));
 	
 	
 	sf::Font consolasFont;
@@ -33,28 +28,29 @@ int main()
 	while (window.isOpen())
 	{
 		//UPDATE
-		sf::Vector2i mousePixelPosition = sf::Mouse::getPosition();
+		sf::Vector2i mousePixelPosition = sf::Mouse::getPosition(window);
 		sf::Vector2f mouseWorldPosition = window.mapPixelToCoords(mousePixelPosition);
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-			if (event.type == sf::Keyboard::Escape)
+			else if (event.type == sf::Keyboard::Escape)
 				window.close();
-			if (event.type == sf::Mouse::isButtonPressed(sf::Mouse::Button))
+			else if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (shape.getGlobalBounds().contains(mouseWorldPosition)) {
-					std::cout << "clicked on circle \n";
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					if (startText.getGlobalBounds().contains(mouseWorldPosition)) {
+						std::cout << "start \n";
+					}
 				}
-				else std::cout << "failed to click \n";
 			}
 		}
 		// ^ UPDATE
 
 		//DRAW
 		window.clear(sf::Color(20, 30, 50));
-		window.draw(shape);
 		window.draw(startText);
 		window.display();
 
